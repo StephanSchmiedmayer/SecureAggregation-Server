ec7425a489c91e2c5c301a286143dd08ec3fbfce
Stephan Schmiedmayer <ge25bih@mytum.de>
2021-07-11 01:41:29 +0200

    Add library

--- Package.swift
+++ Package.swift
@@ -6,6 +6,10 @@ let package = Package(
     platforms: [
        .macOS(.v10_15)
     ],
+    products: [
+        .library(name: "SecureAggregationServer",
+                 targets: ["App"]),
+    ],
     dependencies: [
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.0.0"),
