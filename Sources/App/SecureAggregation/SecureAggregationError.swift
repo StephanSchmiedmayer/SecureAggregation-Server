431d686140d7a11d5e8ed8da93903860af2dbf01
Stephan Schmiedmayer <ge25bih@mytum.de>
2021-09-07 20:04:34 +0200

    Add SecureAggregationError

---
+++ Sources/App/SecureAggregation/SecureAggregationError.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by stephan on 15.08.21.
+//
+
+import Foundation
+
+enum SecureAggregationError: Error {
+    case incorrectStateForMethod
+    case invalidStateTransition
+}
